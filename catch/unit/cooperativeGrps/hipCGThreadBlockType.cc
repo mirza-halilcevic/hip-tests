4c33f4a46343b0e98e32fe5774469d13bf379e31
Dino Music <dino.music@htecgroup.com>
2023-01-29 21:15:26 +0100

    Work on thread block sync test

--- catch/unit/cooperativeGrps/hipCGThreadBlockType.cc
+++ catch/unit/cooperativeGrps/hipCGThreadBlockType.cc
@@ -184,3 +184,102 @@ TEST_CASE("Unit_Thread_Block_Getters_Via_Non_Member_Functions") {
   ArrayAllOf(uint_arr.ptr(), grid.thread_count_,
              [&grid](uint32_t i) { return grid.thread_rank_in_block(i).value(); });
 }
+
+
+__device__ void busy_wait(unsigned long long wait_period) {
+  unsigned long long time_diff = 0;
+  unsigned long long last_clock = clock64();
+  while (time_diff < wait_period) {
+    unsigned long long cur_clock = clock64();
+    if (cur_clock > last_clock) {
+      time_diff += (cur_clock - last_clock);
+    }
+    last_clock = cur_clock;
+  }
+}
+
+template <bool use_global, typename T>
+__global__ void thread_block_sync_check(T* global_data, unsigned int* wait_modifiers,
+                                        unsigned int* read_offsets) {
+  extern __shared__ uint8_t shared_data[];
+  T* const data = use_global ? global_data : reinterpret_cast<T*>(shared_data);
+  const auto block = cg::this_thread_block();
+  constexpr T divisor = 255;
+  const auto tid = block.thread_rank();
+  const auto wait_modifier = wait_modifiers[tid];
+  const auto read_offset = read_offsets[tid];
+  busy_wait(wait_modifier * 100'000);
+  data[tid] = tid % divisor;
+  block.sync();
+  bool valid = true;
+  for (auto i = 0; i < block.size(); ++i) {
+    const auto offset = block.size() + read_offset;
+    const auto expected = (tid + offset + i) % block.size();
+    if (!(valid &= (data[expected] == expected % divisor))) {
+      break;
+    }
+  }
+  block.sync();
+  data[tid] = valid;
+  if constexpr (!use_global) {
+    global_data[tid] = data[tid];
+  }
+}
+
+static inline std::mt19937& GetRandomGenerator() {
+  // With a static seed the tests will remain consistent between runs, yet it relieves the problem
+  // of predetermining a set of modifiers by hand. The sets of modifiers could actually be
+  // determined at compile time if std::random objects could operate in a constexpr context.
+  static std::mt19937 mt(17);
+  return mt;
+}
+
+template <typename T> static inline T GenerateRandomInteger(const T min, const T max) {
+  std::uniform_int_distribution<T> dist(min, max);
+  return dist(GetRandomGenerator());
+}
+
+template <bool global_memory, typename T> void ThreadBlockSyncTest() {
+  const auto randomized_run_count = GENERATE(range(0, 5));
+  const auto threads = dim3(1024, 1, 1);
+  const auto blocks = dim3(1, 1, 1);
+  CPUGrid grid(blocks, threads);
+
+  const auto alloc_size = grid.thread_count_ * sizeof(T);
+  LinearAllocGuard<T> arr_dev(LinearAllocs::hipMalloc, alloc_size);
+  LinearAllocGuard<T> arr(LinearAllocs::hipHostMalloc, alloc_size);
+
+  LinearAllocGuard<unsigned int> wait_modifiers_dev(LinearAllocs::hipMalloc,
+                                                    grid.thread_count_ * sizeof(unsigned int));
+  LinearAllocGuard<unsigned int> wait_modifiers(LinearAllocs::hipHostMalloc,
+                                                grid.thread_count_ * sizeof(unsigned int));
+  std::generate(wait_modifiers.ptr(), wait_modifiers.ptr() + grid.thread_count_,
+                [&] { return GenerateRandomInteger(0u, 10'000u); });
+
+  LinearAllocGuard<unsigned int> read_offsets_dev(LinearAllocs::hipMalloc,
+                                                  grid.thread_count_ * sizeof(unsigned int));
+  std::vector<unsigned int> read_offsets(grid.thread_count_, 0u);
+  if (randomized_run_count != 0) {
+    std::generate(read_offsets.begin(), read_offsets.end(),
+                  [&] { return GenerateRandomInteger(0u, grid.thread_count_); });
+  }
+
+  const auto shared_memory_size = global_memory ? 0u : alloc_size;
+  HIP_CHECK(hipMemcpy(wait_modifiers_dev.ptr(), wait_modifiers.ptr(),
+                      grid.thread_count_ * sizeof(unsigned int), hipMemcpyHostToDevice));
+  HIP_CHECK(hipMemcpy(read_offsets_dev.ptr(), read_offsets.data(),
+                      grid.thread_count_ * sizeof(unsigned int), hipMemcpyHostToDevice));
+
+  thread_block_sync_check<global_memory><<<blocks, threads, shared_memory_size>>>(
+      arr_dev.ptr(), wait_modifiers_dev.ptr(), read_offsets_dev.ptr());
+
+  HIP_CHECK(hipMemcpy(arr.ptr(), arr_dev.ptr(), alloc_size, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipDeviceSynchronize());
+
+  REQUIRE(std::all_of(arr.ptr(), arr.ptr() + grid.thread_count_, [](unsigned int e) { return e; }));
+}
+
+TEMPLATE_TEST_CASE("Blahem", "", uint8_t, uint16_t, uint32_t) {
+  SECTION("Global memory") { ThreadBlockSyncTest<true, TestType>(); }
+  SECTION("Shared memory") { ThreadBlockSyncTest<false, TestType>(); }
+}

b4eabe98085301bf25ac9d936ea2379b7b519bd6
Miloš Mozetić <milos.mozetic@htecgroup.com>
2023-05-12 08:33:19 +0000

    EXSWHTEC-275 - Implement negative compilation tests for atomicCAS

--- catch/unit/atomics/atomicCAS.cc
+++ catch/unit/atomics/atomicCAS.cc
@@ -21,6 +21,7 @@ THE SOFTWARE.
 */
 
 #include "arithmetic_common.hh"
+#include "atomicCAS_negative_kernels_rtc.hh"
 
 #include <hip_test_common.hh>
 
@@ -117,4 +118,46 @@ TEMPLATE_TEST_CASE("Unit_atomicCAS_Positive_Multi_Kernel", "", int, unsigned int
     SingleDeviceMultipleKernelTest<TestType, AtomicOperation::kCASAdd>(2, warp_size,
                                                                        cache_line_size);
   }
+}
+
+/**
+ * Test Description
+ * ------------------------
+ *    - RTCs kernels that pass combinations of arguments of invalid types for all overloads of
+ * atomicCAS.
+ * Test source
+ * ------------------------
+ *    - unit/atomics/atomicCAS.cc
+ * Test requirements
+ * ------------------------
+ *    - HIP_VERSION >= 5.2
+ */
+TEST_CASE("Unit_atomicCAS_Negative_Parameters_RTC") {
+  hiprtcProgram program{};
+
+  const auto program_source = GENERATE(kAtomicCAS_int, kAtomicCAS_uint, kAtomicCAS_ulong,
+                                       kAtomicCAS_ulonglong, kAtomicCAS_float, kAtomicCAS_double);
+  HIPRTC_CHECK(
+      hiprtcCreateProgram(&program, program_source, "atomicCAS_negative.cc", 0, nullptr, nullptr));
+  hiprtcResult result{hiprtcCompileProgram(program, 0, nullptr)};
+
+  // Get the compile log and count compiler error messages
+  size_t log_size{};
+  HIPRTC_CHECK(hiprtcGetProgramLogSize(program, &log_size));
+  std::string log(log_size, ' ');
+  HIPRTC_CHECK(hiprtcGetProgramLog(program, log.data()));
+  int error_count{0};
+
+  int expected_error_count{8};
+  std::string error_message{"error:"};
+
+  size_t n_pos = log.find(error_message, 0);
+  while (n_pos != std::string::npos) {
+    ++error_count;
+    n_pos = log.find(error_message, n_pos + 1);
+  }
+
+  HIPRTC_CHECK(hiprtcDestroyProgram(&program));
+  HIPRTC_CHECK_ERROR(result, HIPRTC_ERROR_COMPILATION);
+  REQUIRE(error_count == expected_error_count);
 }

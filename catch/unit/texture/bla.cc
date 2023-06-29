dee34f381e22f41313a32abaf678e3aba12e88eb
Dino Musić <dino.music@htecgroup.com>
2023-06-29 11:57:36 +0000

    Implement basic example

---
+++ catch/unit/texture/bla.cc
@@ -0,0 +1,56 @@
+#include <vector>
+
+#include <hip_test_common.hh>
+
+
+template<typename Vec>
+__global__ void kernel(hipTextureObject_t tex_obj) {
+  const auto v = tex1DLod<Vec>(tex_obj, 1, 0);
+  printf("%u\n", v.x);
+  printf("%u\n", v.y);
+  printf("%u\n", v.z);
+  printf("%u\n", v.w);
+}
+
+TEST_CASE("Bla") {
+  const int height = 1;
+  const int width = 1024;
+
+  using T = HIP_vector_type<unsigned int, 4>;
+
+  std::vector<T> h_data(width * height);
+  for (auto i = 0u; i < h_data.size(); ++i) {
+    h_data[i].x = i;
+    h_data[i].y = i;
+    h_data[i].z = i;
+    h_data[i].w = i;
+  }
+
+  hipChannelFormatDesc channel_desc = hipCreateChannelDesc<T>();
+
+  hipArray_t hip_arr;
+  HIP_CHECK(hipMallocArray(&hip_arr, &channel_desc, width, height));
+
+  const size_t spitch = width * sizeof(T);
+  HIP_CHECK(hipMemcpy2DToArray(hip_arr, 0, 0, h_data.data(), spitch, spitch, height,
+                               hipMemcpyHostToDevice));
+
+  hipResourceDesc res_desc;
+  memset(&res_desc, 0, sizeof(res_desc));
+  res_desc.resType = hipResourceTypeArray;
+  res_desc.res.array.array = hip_arr;
+
+  hipTextureDesc tex_desc;
+  memset(&tex_desc, 0, sizeof(tex_desc));
+  tex_desc.addressMode[0] = hipAddressModeWrap;
+  tex_desc.addressMode[1] = hipAddressModeWrap;
+  tex_desc.filterMode = hipFilterModePoint;
+  tex_desc.readMode = hipReadModeElementType;
+  tex_desc.normalizedCoords = 0;
+
+  hipTextureObject_t tex_obj = 0;
+  HIP_CHECK(hipCreateTextureObject(&tex_obj, &res_desc, &tex_desc, nullptr));
+
+  kernel<T><<<1, 1>>>(tex_obj);
+  HIP_CHECK(hipDeviceSynchronize());
+}

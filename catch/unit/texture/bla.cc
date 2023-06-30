943b838c2a71e23be016b33b4eb9b5a228fe8b5e
Dino Music <dino.music@htecgroup.com>
2023-06-30 16:20:16 +0200

    Implement basic test for tex1Dfetch

--- catch/unit/texture/bla.cc
+++ catch/unit/texture/bla.cc
@@ -1,13 +1,88 @@
 #include <vector>
 
 #include <hip_test_common.hh>
+#include <cooperative_groups.h>
+#include <resource_guards.hh>
 
 #include "vec4.hh"
 
+namespace cg = cooperative_groups;
 
-template<typename Vec>
-__global__ void kernel(hipTextureObject_t tex_obj) {
-  const auto v = tex1DLod<Vec>(tex_obj, 1, 0);
+class TextureGuard {
+ public:
+  TextureGuard(hipResourceDesc* res_desc, hipTextureDesc* tex_desc) {
+    HIP_CHECK(hipCreateTextureObject(&tex_obj_, res_desc, tex_desc, nullptr));
+  }
+
+  ~TextureGuard() { static_cast<void>(hipDestroyTextureObject(tex_obj_)); }
+
+  TextureGuard(TextureGuard&&) = delete;
+  TextureGuard(const TextureGuard&) = delete;
+
+  hipTextureObject_t object() const { return tex_obj_; }
+
+ private:
+  hipTextureObject_t tex_obj_ = 0;
+};
+
+
+template <typename TexelType>
+__global__ void tex1DfetchKernel(TexelType* const out, hipTextureObject_t tex_obj) {
+  const auto tid = cg::this_grid().thread_rank();
+  out[tid] = tex1Dfetch<TexelType>(tex_obj, tid);
+}
+
+TEST_CASE("tex1Dfetch") {
+  using T = unsigned int;
+  using TexelType = vec4<T>;
+
+  const auto width = 1024;
+
+  std::vector<TexelType> tex_alloc_h(width);
+  for (auto i = 0u; i < tex_alloc_h.size(); ++i) {
+    SetVec4<T>(tex_alloc_h[i], i);
+  }
+
+  LinearAllocGuard<TexelType> tex_alloc_d(LinearAllocs::hipMalloc, width * sizeof(TexelType));
+  HIP_CHECK(hipMemcpy(tex_alloc_d.ptr(), tex_alloc_h.data(), tex_alloc_h.size() * sizeof(TexelType),
+                      hipMemcpyHostToDevice));
+
+  LinearAllocGuard<TexelType> out_alloc_d(LinearAllocs::hipMalloc, width * sizeof(TexelType));
+
+  hipResourceDesc res_desc;
+  memset(&res_desc, 0, sizeof(res_desc));
+  res_desc.resType = hipResourceTypeLinear;
+  res_desc.res.linear.devPtr = tex_alloc_d.ptr();
+  res_desc.res.linear.desc = hipCreateChannelDesc<TexelType>();
+  res_desc.res.linear.sizeInBytes = width * sizeof(TexelType);
+
+  hipTextureDesc tex_desc;
+  memset(&tex_desc, 0, sizeof(tex_desc));
+  tex_desc.addressMode[0] = hipAddressModeClamp;
+  tex_desc.filterMode = hipFilterModePoint;
+  tex_desc.readMode = hipReadModeElementType;
+  tex_desc.normalizedCoords = false;
+
+  TextureGuard tex(&res_desc, &tex_desc);
+  tex1DfetchKernel<TexelType><<<1, width>>>(out_alloc_d.ptr(), tex.object());
+
+  std::vector<TexelType> out_alloc_h(width);
+  HIP_CHECK(hipDeviceSynchronize());
+
+  HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), width * sizeof(TexelType),
+                      hipMemcpyDeviceToHost));
+
+  for (auto i = 0u; i < out_alloc_h.size(); ++i) {
+    REQUIRE(tex_alloc_h[i].x == out_alloc_h[i].x);
+    REQUIRE(tex_alloc_h[i].y == out_alloc_h[i].y);
+    REQUIRE(tex_alloc_h[i].z == out_alloc_h[i].z);
+    REQUIRE(tex_alloc_h[i].w == out_alloc_h[i].w);
+  }
+}
+
+
+template <typename Vec> __global__ void kernel(hipTextureObject_t tex_obj) {
+  const auto v = tex1D<Vec>(tex_obj, 1);
   printf("%u\n", v.x);
   printf("%u\n", v.y);
   printf("%u\n", v.z);

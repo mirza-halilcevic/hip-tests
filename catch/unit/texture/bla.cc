5640c16be13e5de5e5365747c621eb3d8c258dfe
Dino Music <dino.music@htecgroup.com>
2023-07-04 10:27:21 +0200

    Blabla

--- catch/unit/texture/bla.cc
+++ catch/unit/texture/bla.cc
@@ -25,29 +25,68 @@ class TextureGuard {
   hipTextureObject_t tex_obj_ = 0;
 };
 
+template <typename TexelType, typename UnderlyingType> class TextureReference {
+ public:
+  TextureReference(size_t width)
+      : width_{width}, host_alloc_{LinearAllocs::hipHostMalloc, width * sizeof(TexelType)} {}
+
+  template <typename F> void Fill(F f) {
+    for (auto i = 0u; i < width_; ++i) {
+      ptr()[i] = f(i);
+    }
+  }
+
+  TexelType Fetch1D(int x, hipTextureDesc& tex_desc) {
+    x = ApplyAddressMode(x, tex_desc.addressMode[0]);
+    return ptr()[x];
+  }
+
+  TexelType* ptr() { return host_alloc_.ptr(); }
+
+ private:
+  const size_t width_;
+  LinearAllocGuard<TexelType> host_alloc_;
+
+  int ApplyAddressMode(int x, hipTextureAddressMode address_mode) const {
+    switch (address_mode) {
+      case hipAddressModeClamp:
+        return std::min<int>(x, width_);
+      case hipAddressModeBorder:
+        return (x < width_) * x;
+      default:
+        throw "Ded";
+    }
+  }
+};
+
+template <typename T> std::enable_if_t<std::is_integral_v<T>, float> NormalizeInteger(const T x) {
+  return x >= static_cast<T>(0) ? static_cast<float>(x) / std::numeric_limits<T>::max()
+                                : -static_cast<float>(x) / std::numeric_limits<T>::min();
+}
 
 template <typename TexelType>
-__global__ void tex1DfetchKernel(TexelType* const out, hipTextureObject_t tex_obj) {
+__global__ void tex1DfetchKernel(TexelType* const out, size_t N, hipTextureObject_t tex_obj) {
   const auto tid = cg::this_grid().thread_rank();
+  if (tid >= N) return;
+
   out[tid] = tex1Dfetch<TexelType>(tex_obj, tid);
 }
 
-TEST_CASE("tex1Dfetch") {
-  using T = unsigned int;
+TEMPLATE_TEST_CASE("tex1Dfetch", "", char, int, unsigned int, float) {
+  using T = TestType;
   using TexelType = vec4<T>;
 
   const auto width = 1024;
 
-  std::vector<TexelType> tex_alloc_h(width);
-  for (auto i = 0u; i < tex_alloc_h.size(); ++i) {
-    SetVec4<T>(tex_alloc_h[i], i);
-  }
+  TextureReference<TexelType, T> tex_h(width);
+  // TODO - Need some negative values for signed types.
+  tex_h.Fill([](size_t x) { return MakeVec4<T>(x); });
 
   LinearAllocGuard<TexelType> tex_alloc_d(LinearAllocs::hipMalloc, width * sizeof(TexelType));
-  HIP_CHECK(hipMemcpy(tex_alloc_d.ptr(), tex_alloc_h.data(), tex_alloc_h.size() * sizeof(TexelType),
-                      hipMemcpyHostToDevice));
+  HIP_CHECK(
+      hipMemcpy(tex_alloc_d.ptr(), tex_h.ptr(), width * sizeof(TexelType), hipMemcpyHostToDevice));
 
-  LinearAllocGuard<TexelType> out_alloc_d(LinearAllocs::hipMalloc, width * sizeof(TexelType));
+  LinearAllocGuard<TexelType> out_alloc_d(LinearAllocs::hipMalloc, 2 * width * sizeof(TexelType));
 
   hipResourceDesc res_desc;
   memset(&res_desc, 0, sizeof(res_desc));
@@ -58,28 +97,132 @@ TEST_CASE("tex1Dfetch") {
 
   hipTextureDesc tex_desc;
   memset(&tex_desc, 0, sizeof(tex_desc));
-  tex_desc.addressMode[0] = hipAddressModeClamp;
-  tex_desc.filterMode = hipFilterModePoint;
-  tex_desc.readMode = hipReadModeElementType;
-  tex_desc.normalizedCoords = false;
 
-  TextureGuard tex(&res_desc, &tex_desc);
-  tex1DfetchKernel<TexelType><<<1, width>>>(out_alloc_d.ptr(), tex.object());
+  SECTION("Clamp") {
+    tex_desc.addressMode[0] = hipAddressModeClamp;
+    tex_desc.filterMode = hipFilterModePoint;
+    tex_desc.readMode = hipReadModeElementType;
+    tex_desc.normalizedCoords = false;
 
-  std::vector<TexelType> out_alloc_h(width);
-  HIP_CHECK(hipDeviceSynchronize());
+    TextureGuard tex(&res_desc, &tex_desc);
+    tex1DfetchKernel<TexelType><<<2, width>>>(out_alloc_d.ptr(), width * 2, tex.object());
 
-  HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), width * sizeof(TexelType),
-                      hipMemcpyDeviceToHost));
+    std::vector<TexelType> out_alloc_h(2 * width);
 
-  for (auto i = 0u; i < out_alloc_h.size(); ++i) {
-    REQUIRE(tex_alloc_h[i].x == out_alloc_h[i].x);
-    REQUIRE(tex_alloc_h[i].y == out_alloc_h[i].y);
-    REQUIRE(tex_alloc_h[i].z == out_alloc_h[i].z);
-    REQUIRE(tex_alloc_h[i].w == out_alloc_h[i].w);
+    HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), 2 * width * sizeof(TexelType),
+                        hipMemcpyDeviceToHost));
+    HIP_CHECK(hipDeviceSynchronize());
+
+    for (auto i = 0u; i < out_alloc_h.size(); ++i) {
+      const auto ref_val = tex_h.Fetch1D(i, tex_desc);
+      REQUIRE(ref_val.x == out_alloc_h[i].x);
+      REQUIRE(ref_val.y == out_alloc_h[i].y);
+      REQUIRE(ref_val.z == out_alloc_h[i].z);
+      REQUIRE(ref_val.w == out_alloc_h[i].w);
+    }
+  }
+
+  SECTION("Border") {
+    tex_desc.addressMode[0] = hipAddressModeBorder;
+    tex_desc.filterMode = hipFilterModePoint;
+    tex_desc.readMode = hipReadModeElementType;
+    tex_desc.normalizedCoords = false;
+
+    TextureGuard tex(&res_desc, &tex_desc);
+    tex1DfetchKernel<TexelType><<<2, width>>>(out_alloc_d.ptr(), width * 2, tex.object());
+
+    std::vector<TexelType> out_alloc_h(2 * width);
+
+    HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), 2 * width * sizeof(TexelType),
+                        hipMemcpyDeviceToHost));
+    HIP_CHECK(hipDeviceSynchronize());
+
+    for (auto i = 0u; i < out_alloc_h.size(); ++i) {
+      const auto ref_val = tex_h.Fetch1D(i, tex_desc);
+      REQUIRE(ref_val.x == out_alloc_h[i].x);
+      REQUIRE(ref_val.y == out_alloc_h[i].y);
+      REQUIRE(ref_val.z == out_alloc_h[i].z);
+      REQUIRE(ref_val.w == out_alloc_h[i].w);
+    }
   }
 }
 
+TEMPLATE_TEST_CASE("tex1Dfetch_normalized", "", char, unsigned char) {
+  using T = TestType;
+  using TexelType = vec4<T>;
+
+  const auto width = 1024;
+
+  TextureReference<TexelType, T> tex_h(width);
+  // TODO - Need some negative values for signed types.
+  tex_h.Fill([](size_t x) { return MakeVec4<T>(x); });
+
+  LinearAllocGuard<TexelType> tex_alloc_d(LinearAllocs::hipMalloc, width * sizeof(TexelType));
+  HIP_CHECK(
+      hipMemcpy(tex_alloc_d.ptr(), tex_h.ptr(), width * sizeof(TexelType), hipMemcpyHostToDevice));
+
+  LinearAllocGuard<vec4<float>> out_alloc_d(LinearAllocs::hipMalloc,
+                                            2 * width * sizeof(vec4<float>));
+
+  hipResourceDesc res_desc;
+  memset(&res_desc, 0, sizeof(res_desc));
+  res_desc.resType = hipResourceTypeLinear;
+  res_desc.res.linear.devPtr = tex_alloc_d.ptr();
+  res_desc.res.linear.desc = hipCreateChannelDesc<TexelType>();
+  res_desc.res.linear.sizeInBytes = width * sizeof(TexelType);
+
+  hipTextureDesc tex_desc;
+  memset(&tex_desc, 0, sizeof(tex_desc));
+
+  SECTION("Clamp") {
+    tex_desc.addressMode[0] = hipAddressModeClamp;
+    tex_desc.filterMode = hipFilterModePoint;
+    tex_desc.readMode = hipReadModeNormalizedFloat;
+    tex_desc.normalizedCoords = false;
+
+    TextureGuard tex(&res_desc, &tex_desc);
+    tex1DfetchKernel<vec4<float>><<<2, width>>>(out_alloc_d.ptr(), width * 2, tex.object());
+
+    std::vector<vec4<float>> out_alloc_h(2 * width);
+
+    HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), 2 * width * sizeof(vec4<float>),
+                        hipMemcpyDeviceToHost));
+    HIP_CHECK(hipDeviceSynchronize());
+
+    for (auto i = 0u; i < out_alloc_h.size(); ++i) {
+      INFO("i: " << i);
+      const auto ref_val = tex_h.Fetch1D(i, tex_desc);
+      CHECK(NormalizeInteger(ref_val.x) == out_alloc_h[i].x);
+      // REQUIRE(ref_val.y == out_alloc_h[i].y);
+      // REQUIRE(ref_val.z == out_alloc_h[i].z);
+      // REQUIRE(ref_val.w == out_alloc_h[i].w);
+    }
+  }
+
+  // SECTION("Border") {
+  //   tex_desc.addressMode[0] = hipAddressModeBorder;
+  //   tex_desc.filterMode = hipFilterModePoint;
+  //   tex_desc.readMode = hipReadModeNormalizedFloat;
+  //   tex_desc.normalizedCoords = false;
+
+  //   TextureGuard tex(&res_desc, &tex_desc);
+  //   tex1DfetchKernel<vec4<float>><<<2, width>>>(out_alloc_d.ptr(), width * 2, tex.object());
+
+  //   std::vector<vec4<float>> out_alloc_h(2 * width);
+
+  //   HIP_CHECK(hipMemcpy(out_alloc_h.data(), out_alloc_d.ptr(), 2 * width * sizeof(TexelType),
+  //                       hipMemcpyDeviceToHost));
+  //   HIP_CHECK(hipDeviceSynchronize());
+
+  //   for (auto i = 0u; i < out_alloc_h.size(); ++i) {
+  //     const auto ref_val = tex_h.Fetch1D(i, tex_desc);
+  //     REQUIRE(ref_val.x == out_alloc_h[i].x);
+  //     REQUIRE(ref_val.y == out_alloc_h[i].y);
+  //     REQUIRE(ref_val.z == out_alloc_h[i].z);
+  //     REQUIRE(ref_val.w == out_alloc_h[i].w);
+  //   }
+  // }
+}
 
 template <typename Vec> __global__ void kernel(hipTextureObject_t tex_obj) {
   const auto v = tex1D<Vec>(tex_obj, 1);

ce69bf6f7882ed13988c2d7b4b0edd484a5d81c8
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2023-07-04 14:15:30 +0000

    bla

--- catch/unit/texture/bla.cc
+++ catch/unit/texture/bla.cc
@@ -31,7 +31,7 @@ THE SOFTWARE.
 #include "vec4.hh"
 
 template <typename Vec> __global__ void kernel(hipTextureObject_t tex_obj) {
-  const auto v = tex1D<Vec>(tex_obj, 1);
+  const auto v = tex1D<Vec>(tex_obj, 1025);
   printf("%u\n", v.x);
   printf("%u\n", v.y);
   printf("%u\n", v.z);
@@ -53,25 +53,26 @@ TEST_CASE("Bla") {
 
   hipChannelFormatDesc channel_desc = hipCreateChannelDesc<T>();
 
+  ArrayAllocGuard<T> arr_alloc(make_hipExtent(1024, 0, 0));
+
   hipArray_t hip_arr;
   HIP_CHECK(hipMallocArray(&hip_arr, &channel_desc, width, height));
 
   const size_t spitch = width * sizeof(T);
-  HIP_CHECK(hipMemcpy2DToArray(hip_arr, 0, 0, h_data.data(), spitch, spitch, height,
+  HIP_CHECK(hipMemcpy2DToArray(arr_alloc.ptr(), 0, 0, h_data.data(), spitch, spitch, height,
                                hipMemcpyHostToDevice));
 
   hipResourceDesc res_desc;
   memset(&res_desc, 0, sizeof(res_desc));
   res_desc.resType = hipResourceTypeArray;
-  res_desc.res.array.array = hip_arr;
+  res_desc.res.array.array = arr_alloc.ptr();
 
   hipTextureDesc tex_desc;
   memset(&tex_desc, 0, sizeof(tex_desc));
   tex_desc.addressMode[0] = hipAddressModeWrap;
-  tex_desc.addressMode[1] = hipAddressModeWrap;
   tex_desc.filterMode = hipFilterModePoint;
   tex_desc.readMode = hipReadModeElementType;
-  tex_desc.normalizedCoords = 0;
+  tex_desc.normalizedCoords = false;
 
   hipTextureObject_t tex_obj = 0;
   HIP_CHECK(hipCreateTextureObject(&tex_obj, &res_desc, &tex_desc, nullptr));
